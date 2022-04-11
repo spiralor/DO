c7c74768c3e33e6d56f0b268aae696d54ab1bed1
spiralor <33825103+spiralor@users.noreply.github.com>
2022-04-11 15:32:25 +0800

    Update correlation_matrix_plot.py

--- correlation_matrix_plot.py
+++ correlation_matrix_plot.py
@@ -2,14 +2,15 @@ from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
 
-data_path = 'C:/Users/26982/Desktop/DO Experiment/Indian/GRIDDING_2dbf5_t_seed1_after1970_Indian.csv'
+# data_path = 'C:/Users/26982/Desktop/DO Experiment/Indian/GRIDDING_2dbf5_t_seed1_after1970_Indian.csv'
+data_path = 'C:/Users/26982/Desktop/DO Experiment/Indian/GRIDDING_2dbf5_t.csv'
 df = pd.read_csv(data_path)
 corr = df.corr().round(2).values
 
 f = plt.figure(figsize=(20, 20))
 
 corr_mtrix = plt.matshow(corr, fignum=f.number, cmap='coolwarm')
-cb = plt.colorbar()
+cb = plt.colorbar(fraction=0.046)
 cb.ax.tick_params(labelsize=20)
 xticks_ticks = range(df.columns.shape[0])
 xticks_labels = df.columns.to_numpy()
@@ -18,10 +19,11 @@ yticks_labels = xticks_labels
 plt.xticks(xticks_ticks, xticks_labels, fontsize=20, rotation=90)
 plt.yticks(range(df.select_dtypes(['number']).shape[1]), df.select_dtypes(['number']).columns, fontsize=20)
 
-for row in range(16):
-    for col in range(16):
+for row in range(df.columns.shape[0]):
+    for col in range(df.columns.shape[0]):
         plt.text(row, col, corr[row][col], horizontalalignment='center', verticalalignment='center',
                  fontsize=20)
 
-plt.title('2dbf5_t_seed1_after1970_Indian_Ocean Correlation Matrix', fontsize=40)
+plt.title('2dbf5_t Correlation Matrix', fontsize=40)
+# plt.title('2dbf5_t_seed1_after1970_Indian_Ocean Correlation Matrix', fontsize=40)
 plt.show()
