8788733c8d58df1c2e56fceafc09cd5daf879e2c
spiralor <33825103+spiralor@users.noreply.github.com>
2022-04-11 16:04:37 +0800

    Add files via upload

---
+++ predict/o2_sp_test_full.sh
@@ -0,0 +1,105 @@
+oceanlist=('Antarctic' 'Arctic' 'Atlantic' 'Indian' 'Pacific')
+targetPath='../cdo_'$*''
+mkdir -p ${targetPath}/o2_spatial_depth/
+mkdir -p ${targetPath}/o2_storage_depth/
+mkdir -p ${targetPath}/o2_vertmin/
+for file in ../$*/*
+do
+	{
+		file_name=${file##*/}
+		file_year=${file_name%%_*}
+		cdo -z zip -mul -expr,'o2s = o2_pred_rf/1000/0.9737' -timmean ${file} full_depth_height.nc ${targetPath}/o2_spatial_depth/${file_year}_o2_spatial_d.nc
+		cdo -mul -fldsum -expr,'o2P = o2_pred_rf/1000000000000000000/0.9737' -mul ${file} gr_area.nc depth_height.nc ${targetPath}/o2_storage_depth/${file_year}_o2_Pmol.nc
+		cdo -z zip -vertmin -timmin ${file} ${targetPath}/o2_vertmin/${file_year}_vertmin.nc
+		echo ${file_year}
+		
+		
+	}&
+done
+wait
+#逐层变化量 mol m-2
+cdo -z zip -mergetime ${targetPath}/o2_spatial_depth/*.nc ${targetPath}/o2_spatial_d.nc
+#逐层储量(月平均) Pmol
+cdo -mergetime ${targetPath}/o2_storage_depth/*.nc ${targetPath}/o2P_d_m.nc
+#水柱最低浓度
+cdo -z zip -mergetime ${targetPath}/o2_vertmin/*.nc ${targetPath}/o2_spatial_vertmin.nc &
+
+#总空间变化量 mol m-2
+cdo -z zip -vertsum ${targetPath}/o2_spatial_d.nc ${targetPath}/o2_spatial.nc
+#1000以上空间变化量 mol m-2
+cdo -z zip -vertsum -sellevidx,1/22 ${targetPath}/o2_spatial_d.nc ${targetPath}/o2_spatial_0_1000.nc
+#1000以下空间变化量 mol m-2
+cdo -z zip -vertsum -sellevidx,23/40 ${targetPath}/o2_spatial_d.nc ${targetPath}/o2_spatial_1000_5000.nc
+#1871-2010总趋势 mol m-2 year
+cdo -z zip -trend ${targetPath}/o2_spatial.nc ${targetPath}/o2_spatial_trend_a.nc ${targetPath}/o2_spatial_trend_b.nc &
+#1871-2010 1000以上趋势 mol m-2 year
+cdo -z zip -trend ${targetPath}/o2_spatial_0_1000.nc ${targetPath}/o2_spatial_0_1000_trend_a.nc ${targetPath}/o2_spatial_0_1000_trend_b.nc &
+#1871-2010 1000以下趋势 mol m-2 year
+cdo -z zip -trend ${targetPath}/o2_spatial_1000_5000.nc ${targetPath}/o2_spatial_1000_5000_trend_a.nc ${targetPath}/o2_spatial_1000_5000_trend_b.nc &
+
+#1980-2010趋势 mol m-2 year
+cdo -z zip -trend -selyear,1980/2010 ${targetPath}/o2_spatial.nc ${targetPath}/o2_spatial_trend_1980_a.nc ${targetPath}/o2_spatial_trend_1980_b.nc &
+#1980-2010 1000以上趋势 mol m-2 year
+cdo -z zip -trend -selyear,1980/2010 ${targetPath}/o2_spatial_0_1000.nc ${targetPath}/o2_spatial_0_1000_trend_1980_a.nc ${targetPath}/o2_spatial_0_1000_trend_1980_b.nc &
+#1980-2010 1000以下趋势 mol m-2 year
+cdo -z zip -trend -selyear,1980/2010 ${targetPath}/o2_spatial_1000_5000.nc ${targetPath}/o2_spatial_1000_5000_trend_1980_a.nc ${targetPath}/o2_spatial_1000_5000_trend_1980_b.nc &
+
+
+#总空间变化量140年平均 mol m-2
+cdo -z zip -timmean ${targetPath}/o2_spatial.nc ${targetPath}/o2_spatial_timmean.nc
+#总空间变化量距平 mol m-2
+cdo -z zip -sub ${targetPath}/o2_spatial.nc ${targetPath}/o2_spatial_timmean.nc ${targetPath}/o2_spatial-timmean.nc &
+
+#逐层储量 Pmol
+cdo -yearmean ${targetPath}/o2P_d_m.nc ${targetPath}/o2P_d.nc &
+#总储量 Pmol
+cdo -expr,'o2P = o2s/1000000000000000' -fldsum -mul ${targetPath}/o2_spatial.nc gr_area.nc ${targetPath}/o2P.nc &
+#<1000米储量 Pmol
+cdo -expr,'o2P = o2s/1000000000000000' -fldsum -mul ${targetPath}/o2_spatial_0_1000.nc gr_area.nc ${targetPath}/o2P_0_1000.nc &
+#>1000米储量 Pmol
+cdo -expr,'o2P = o2s/1000000000000000' -fldsum -mul ${targetPath}/o2_spatial_1000_5000.nc gr_area.nc ${targetPath}/o2P_1000_5000.nc &
+
+#各海区储量 Pmol
+for oceanname in ${oceanlist[@]}
+do	{
+		#总储量
+		cdo -expr,'o2P = o2s/1000000000000000' -fldsum  -mul ${targetPath}/o2_spatial.nc ${oceanname}_area.nc ${targetPath}/o2P_${oceanname}.nc
+		#<1000米
+		cdo -expr,'o2P = o2s/1000000000000000' -fldsum  -mul ${targetPath}/o2_spatial_0_1000.nc ${oceanname}_area.nc ${targetPath}/o2P_${oceanname}_0_1000.nc
+		#>1000米
+		cdo -expr,'o2P = o2s/1000000000000000' -fldsum  -mul ${targetPath}/o2_spatial_1000_5000.nc ${oceanname}_area.nc ${targetPath}/o2P_${oceanname}_1000_5000.nc
+	}&
+done
+wait
+
+mkdir -p ${targetPath}/o2c_mmol_m-3_spatial/
+mkdir -p ${targetPath}/o2c_μmol_kg-1_spatial_0_1200/
+mkdir -p ${targetPath}/o2c_μmol_kg-1_spatial_1200_5000/
+
+for file in ../$*/*
+do
+        {
+                file_name=${file##*/}
+                file_year=${file_name%%_*}
+                cdo -z zip -expr,'o2c = o2_pred_rf * 1.027' -intlevel,100,400,700 -timmean ${file} ${targetPath}/o2c_mmol_m-3_spatial/${file_year}_o2c_spatial.nc
+                cdo -z zip -expr,'o2c = o2_pred_rf * 1' -vertmean -sellevidx,1/23 -timmean ${file} ${targetPath}/o2c_μmol_kg-1_spatial_0_1200/${file_year}_o2c_spatial_0_1200.nc
+                cdo -z zip -expr,'o2c = o2_pred_rf * 1' -vertmean -sellevidx,23/40 -timmean ${file} ${targetPath}/o2c_μmol_kg-1_spatial_1200_5000/${file_year}_o2c_spatial_1200_5000.nc
+                echo ${file_year}
+        }&
+done
+wait
+
+#100 400 700变化时间合并 mmol m-3
+cdo -z zip -mergetime ${targetPath}/o2c_mmol_m-3_spatial/*.nc ${targetPath}/o2c_mmol_m-3_spatial.nc
+#0-1200 1200-5000变化时间合并 μmol kg-1
+cdo -z zip -mergetime ${targetPath}/o2c_μmol_kg-1_spatial_0_1200/*.nc ${targetPath}/o2c_μmol_kg-1_spatial_0_1200.nc
+cdo -z zip -mergetime ${targetPath}/o2c_μmol_kg-1_spatial_1200_5000/*.nc ${targetPath}/o2c_μmol_kg-1_spatial_1200_5000.nc
+#1958-2010 trend mmol m-3 year-1
+cdo -z zip -trend -selyear,1958/2010 ${targetPath}/o2c_mmol_m-3_spatial.nc ${targetPath}/o2c_mmol_m-3_spatial_trend_1958_a.nc ${targetPath}/o2c_mmol_m-3_spatial_trend_1958_b.nc &
+
+#1960-2010 trend μmol kg-1 year-1
+cdo -z zip -trend -selyear,1960/2010 ${targetPath}/o2c_μmol_kg-1_spatial_0_1200.nc ${targetPath}/o2c_μmol_kg-1_spatial_0_1200_trend_1960_a.nc ${targetPath}/o2c_μmol_kg-1_spatial_0_1200_trend_1960_b.nc &
+cdo -z zip -trend -selyear,1960/2010 ${targetPath}/o2c_μmol_kg-1_spatial_1200_5000.nc ${targetPath}/o2c_μmol_kg-1_spatial_1200_5000_trend_1960_a.nc ${targetPath}/o2c_μmol_kg-1_spatial_1200_5000_trend_1960_b.nc &
+
+
+echo "finish"
---
+++ predict/predict_50models_OMZ_coslon.py
@@ -0,0 +1,193 @@
+﻿from netCDF4 import Dataset, num2date
+import numpy as np
+import numpy.ma as ma
+import joblib
+import time as tm
+import os
+import gc
+import threading
+from multiprocessing import Pool
+
+def model_predict(tup):
+    yearIndex = tup[0]
+    result_path = tup[1]
+    print(str(yearIndex) + ' start! ' + str(tm.strftime("%Y-%m-%d %H:%M:%S", tm.localtime())))
+
+    DstFilePath = r'{0}_do_predict_zip.nc'.format(str(yearIndex))
+    DstFilePath2 = result_path + DstFilePath
+
+    X = Dataset(to_predict_path + DstFilePath)
+    time_units = X.variables['time'].units
+    time = X.variables['time'][:]
+    year = yearIndex
+    depth = X.variables['depth'][:]
+    lat_new = X.variables['lat'][:]
+    lon_new = X.variables['lon'][:]
+    cos_lon = np.cos(np.pi / 180 * lon_new)
+    taux = X.variables['taux'][:]
+    tauy = X.variables['tauy'][:]
+    ssh = X.variables['ssh'][:]
+    u = X.variables['u'][:]
+    v = X.variables['v'][:]
+    w = X.variables['w'][:]
+    salt = X.variables['salt'][:]
+    temp = X.variables['temp'][:]
+    cmip6_o2 = X.variables['cmip6_cdo_o2'][:]
+    etop = X.variables['etop'][:]
+    X.close()
+
+    Y = Dataset(DstFilePath2, 'w', format='NETCDF4')
+    Y.createDimension('time', 12)
+    Y.createDimension('depth', 40)
+    Y.createDimension('lat', 330)
+    Y.createDimension('lon', 720)
+    Y.createDimension('bnds', 2)
+    if str(Y.variables.keys()).find('o2_pred_rf') == -1:
+        Y.createVariable('o2_pred_rf', 'f', ('time', 'depth', 'lat', 'lon'), fill_value=-9.99E33, zlib=True)
+    if str(Y.variables.keys()).find('depth_bnds') == -1:
+        Y.createVariable('depth_bnds', 'f', ('depth', 'bnds'), fill_value=-9.99E33, zlib=True)
+    Y.createVariable('time', 'd', ('time'), zlib=True)
+    Y.createVariable('depth', 'd', ('depth'), zlib=True)
+    Y.createVariable('lat', 'd', ('lat'), zlib=True)
+    Y.createVariable('lon', 'd', ('lon'), zlib=True)
+    Y.variables['time'][:] = time
+    Y.variables['depth'][:] = depth
+    Y.variables['depth'].bounds = "depth_bnds"
+    Y.variables['lat'][:] = lat_new
+    Y.variables['lon'][:] = lon_new
+    
+    Z = Dataset('1871_do_predict_zip.nc')
+    bnds = Z.variables['depth_bnds'][:]
+    Z.close()
+    Y.variables['depth_bnds'][:] = bnds
+    Y.variables['time'].units = time_units
+    Y.variables['lat'].units = "degrees_north"
+    Y.variables['lon'].units = "degrees_east"
+
+    #for monthIndex in range(0, 12):
+    for monthIndex in [3-1, 6-1, 9-1]:
+        for depthIndex in range(0, 40):
+            month1 = monthIndex + 1
+            _cos_lon = np.tile(cos_lon, 330).reshape(-1, 1).ravel()
+            _lat = np.repeat(lat_new, 720).ravel()
+            _year = np.repeat(year, 330 * 720).ravel()
+            _month1 = np.repeat(month1, 330 * 720).ravel()
+            _depth = np.repeat(depth[depthIndex], 330 * 720).ravel()
+            _u = u[monthIndex, depthIndex, :].ravel()
+            _v = v[monthIndex, depthIndex, :].ravel()
+            _w = w[monthIndex, depthIndex, :].ravel()
+            _temp = temp[monthIndex, depthIndex, :].ravel()
+            _salt = salt[monthIndex, depthIndex, :].ravel()
+            _ssh = ssh[monthIndex, :].ravel()
+            _taux = taux[monthIndex, :].ravel()
+            _tauy = tauy[monthIndex, :].ravel()
+            _etop = etop.ravel()
+            _cmip6 = cmip6_o2[monthIndex, depthIndex, :].ravel()
+            _cmip6[np.isnan(_cmip6)] = -9.98999971e+33
+            X_pred = np.c_[_cos_lon, _lat, _year, _month1, _depth, _u, _v, _w, _temp, _salt, _ssh, _taux, _tauy, _etop, _cmip6]
+            X_mask_data = np.c_[_u, _v, _w, _temp, _salt, _ssh, _taux, _tauy, _cmip6]
+
+            mask_all = np.matrix(X_mask_data < -9e+33)
+            mask_mul = np.matrix([True, True, True, True, True, True, True, True, True]).T
+            mask = np.matmul(mask_all, mask_mul)
+
+            judge_index = np.where(mask == False)
+            y_pred = np.zeros(len(mask))
+            y_pred[judge_index[0]] = model.predict(X_pred[judge_index[0],:])
+            y_pred[y_pred < 0] = 0
+            y_pred = ma.masked_array(y_pred, mask=mask)
+            y_pred = y_pred.reshape(330, 720)
+            Y.variables['o2_pred_rf'][monthIndex, depthIndex, :, :] = y_pred
+    Y.close()
+    print(str(yearIndex) + ' end! ' + str(tm.strftime("%Y-%m-%d %H:%M:%S", tm.localtime())))
+
+def cal_OMZ(index):
+
+    file_name = '{0}_do_predict_zip.nc'.format(index)
+    # OMZ 20
+    range_shell = 'cdo -z zip -lec,20 -vertmin -timmin {1}/{0}_do_predict_zip.nc {2}/{0}_omz_select_region.nc'.format(index, model_path,dst_path_20)
+    os.system(range_shell)
+    area_shell = 'cdo -z zip -fldsum -mul {1}/{0}_omz_select_region.nc {3}/gr_area.nc {2}/{0}_omz_area.nc'.format(index, dst_path_20, dst_path_20_area, tool_path)
+    os.system(area_shell)
+    
+    return 
+
+
+
+if __name__ == '__main__':
+    
+    model_list = ['et_200_none_2_fit_2db_v0_i0_b0_seed1_after1960_cos_lon.pkl']
+                          
+    for model_item in model_list:
+        model_pkl = model_item
+        model_name = os.path.splitext(model_pkl)[0]
+        RF_model_path = r'../../model/' + model_pkl
+        to_predict_path = r'../nc_predict_zip_dataset/'
+        result_path = r'../' + model_name + '/'
+        if not os.path.exists(result_path):
+            os.makedirs(result_path)
+
+        model = joblib.load(RF_model_path)
+        model.set_params(verbose=0,n_jobs=-1)
+        print('load model ' + str(RF_model_path))
+        
+        items = [ i for i in range(1871,2011)]
+        tup = np.c_[items, np.repeat(result_path, len(items))]
+        pool = Pool(80)
+        pool.map(model_predict, tup)
+        pool.close()
+        pool.join()
+       
+
+        cdoshell = 'bash ./o2_sp_test.sh {0}'.format(model_name)
+        val = os.system(cdoshell) 
+        
+        # 计算OMZ
+        model_path = '../' + model_name
+        dst_path_20 =  '../cdo_{0}/OMZ_20'.format(model_name)
+        tool_path = '/home/zju/do4/do_predict/predict/'
+        if not os.path.exists(dst_path_20):
+            os.makedirs(dst_path_20)            
+        dst_path_20_area = '../cdo_{0}/OMZ_20/OMZ_20_area'.format(model_name)
+        if not os.path.exists(dst_path_20_area):
+            os.makedirs(dst_path_20_area)   
+
+        year = items
+        pool = Pool()
+        pool.map(cal_OMZ, year)
+        pool.close()
+        pool.join()            
+            
+        # 合并OMZ_20_area
+        shell_20 = 'cdo mergetime {0}/*.nc {0}/OMZ_area_timeseries.nc'.format(dst_path_20_area)
+        os.system(shell_20)
+        
+        # 统计OMZ_20出现频次
+        shell = 'cdo enssum '
+        for year_index in year:
+            shell += ' {0}/{1}_omz_select_region.nc '.format(dst_path_20,year_index)
+        shell += ' {0}/OMZ_20_freqcy.nc'.format(dst_path_20)
+        os.system(shell)
+        
+        # 统计最早OMZ_20出现时间    
+        for year_index in year:
+            file_name = '{0}_omz_select_region.nc'.format(year_index)
+            shell ='cdo -setctomiss,0 -expr,"year_label = o2_pred_rf*{0}"  {1}/{2}  {1}/{0}_omz_select_region_year_label.nc'.format(year_index,dst_path_20,file_name)
+            os.system(shell)
+            
+        shell = 'cdo -ensmin '
+        for year_index in year:
+            file_name = ' {1}/{0}_omz_select_region_year_label.nc '.format(year_index,dst_path_20)
+            shell += file_name
+        shell += ' {0}/OMZ_start_yr.nc'.format(dst_path_20)
+        print(shell)
+        
+        os.system(shell)
+        
+        delete_shell = '\ncp -r ../cdo_{0} /home/zju/do4/do_predict/predict/50_ANALYZE/' \
+                '\ncp -r ../{0} /home/zju/do4/do_predict/predict/50_RESULT/' \
+                '\nrm -rf ../{0}' \
+                '\nrm -rf ../cdo_{0}' \
+                '\necho \"finish\"'.format(model_name)        
+        val = os.system(delete_shell)           
+        print( model_name + ' end! ' + str(tm.strftime("%Y-%m-%d %H:%M:%S", tm.localtime())))
