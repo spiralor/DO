dca79ab54e66beac0e100a63ad9c9f3ddc5b5aff
spiralor <33825103+spiralor@users.noreply.github.com>
2022-07-13 22:27:35 +0800

    Update make_depth_file.py

--- data_prepare/soda342/make_depth_file.py
+++ data_prepare/soda342/make_depth_file.py
@@ -35,6 +35,49 @@ def make_grid_nc_file():
     nc_depth.close()
     return
 
+def make_grid_nc_file_Indian():
+    # 读取海洋深度文件
+    nc_file_path = r'E:\do4\soda342\soda3.4.2_mn_ocean_reg_1980.nc'
+    nc_file = Dataset(nc_file_path, mode='r')
+    depth = nc_file.variables['st_ocean'][:]
+
+    lat_s = nc_file.variables['yt_ocean'][:]
+    lon_s = nc_file.variables['xt_ocean'][:]
+    lon_min = 21.75
+    lon_max = 151.25
+    lat_min = -49.75
+    lat_max = 26.75
+    lon_min_index = np.where(lon_s == lon_min)[0][0]
+    lon_max_index = np.where(lon_s == lon_max)[0][0] + 1
+    lat_min_index = np.where(lat_s == lat_min)[0][0]
+    lat_max_index = np.where(lat_s == lat_max)[0][0] + 1
+    lon_dimension = int((lon_max - lon_min)/0.5 + 1)
+    lat_dimension = int((lat_max - lat_min)/0.5 + 1)
+
+    lat = nc_file.variables['yt_ocean'][lat_min_index:lat_max_index]
+    lon = nc_file.variables['xt_ocean'][lon_min_index:lon_max_index]
+
+    # 生成海洋深度文件
+    nc_depth_path = r'E:\do4\soda342_full_depth_height_Indian.nc'
+    # nc_depth_path = r'E:\do4\soda342_full_depth_height_1000.nc'
+    # depth_dimension = 28
+    depth_dimension = 50
+    nc_depth = Dataset(nc_depth_path, mode='w')
+    nc_depth.createDimension('depth', depth_dimension)
+    nc_depth.createDimension('lat', lat_dimension)
+    nc_depth.createDimension('lon', lon_dimension)
+    nc_depth.createVariable('lat', 'f', ('lat'), fill_value=-9.99E33)
+    nc_depth.createVariable('lon', 'f', ('lon'), fill_value=-9.99E33)
+    nc_depth.variables['lat'][:] = lat
+    nc_depth.variables['lon'][:] = lon
+    nc_depth.createVariable('depth_height', 'f', ('depth', 'lat', 'lon'), fill_value=-9.99E33)
+    for depth_index in range(depth_dimension):
+        depth_num = depth[depth_index]
+        print(depth_index, depth_num)
+        nc_depth.variables['depth_height'][depth_index, :, :] = np.repeat(depth_num, lat_dimension * lon_dimension).reshape(lat_dimension, lon_dimension)
+    nc_file.close()
+    nc_depth.close()
+    return
 
 def make_nc_file():
     nc_file_path = r'E:\do4\soda342\soda3.4.2_mn_ocean_reg_1980.nc'
